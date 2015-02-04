a052063de074c192f4c6c7f1f63e73c552c71458
Øyvind Rønne <oyron@statoil.com>
2015-02-04 09:15:57 +0100

    Session store in MongoDb

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
-var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var MongoStore  = require('connect-mongo')(session);
+var cookieParser = require('cookie-parser');
 
 app.use(cookieParser());
 app.use(session({
   secret: '1234567890MODNAR',
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  store : new MongoStore({
+      db: 'sessionstore'
+    })
 }));
 
 var port = process.env.PORT || 9000;
